9218765f0fa4e7c0b94a5f3b1d4263b85018101d
iammaryjulia <iammaryjulia@gmail.com>
2026-08-18 15:14:59 +0800

    OK

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="巨鴻地產房源管理平台", page_icon="🏠", layout="wide")
+
+st.title("🏠 巨鴻地產房源管理平台")
+st.markdown("### 歡迎使用！請從左側選單選擇功能")
+# 可以放公司 Logo、簡介、快速按鈕（房源總覽、今日到期、未收租金等）
